678b7a12f5600acebc94e8a50ccb218834d40ecf
lucasCarvalho0825 <lucascarvalho08.25@gmail.com>
2025-09-07 11:17:52 -0300

    feat: adicionar componente comp-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CompFormComponent } from './components/comp-form/comp-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule,CompFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/comp-form/comp-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-form',
+  imports: [],
+  templateUrl: './comp-form.component.html',
+  styleUrl: './comp-form.component.scss'
+})
+export class CompFormComponent {
+
+}
